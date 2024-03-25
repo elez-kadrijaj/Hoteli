ea613c039e3dd5106623064c19f4363c84a09d29
elez-kadrijaj <ek67709@ubt-uni.net>
2024-03-25 20:55:28 +0100

    Fillimi i Coveri-it

--- Hoteli/src/App.jsx
+++ Hoteli/src/App.jsx
@@ -1,13 +1,17 @@
 import Header from './Header'
 import './App.css'
+import logo from './Eli-Tr.png';
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <Header />
+      <img src={logo} alt="Logo" />
     </>
+    
   )
 }
 
 export default App
+
